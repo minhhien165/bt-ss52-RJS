7e3861c097fca7d25caf7ee567ac0efde96f5d18
minhhien165 <minhhienhd2005@gmail.com>
2024-06-04 13:03:12 +0700

    add bt-SS52

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Bt1/Home";
+import About from "./components/Bt1/About";
+import Contact from "./components/Bt1/Contact";
+import Main1 from "./components/Bt1/Main1";
+import Main3 from "./components/Bt3/Main3";
+
+export default function App() {
+  return (
+    <div>
+      APP
+      <br />
+      {/* <Main1></Main1> */}
+      <Main3></Main3>
+    </div>
+  );
+}
